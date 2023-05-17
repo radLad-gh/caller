a069db8b20cef3e24f3c273e7110e7fbc60207e4
Justice Smith <jcode94ttv@gmail.com>
2023-05-17 14:27:37 -0400

    rm gitignore, allow go.sum

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/radLad-gh/caller
 
 go 1.16
+
+require github.com/radLad-gh/sample v0.0.0-20230517155913-16cc08fbef3f
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	
-	sample "github.com/radLad-gh/sample"
+
+	sample "github.com/radLad-gh/sample/src/util"
 )
 
 func main() {
 	fmt.Println(sample.About())
+	fmt.Println(sample.Hello("Justice"))
+	fmt.Println(sample.GetQuote())
 }
